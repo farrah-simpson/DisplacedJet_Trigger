42b8abd867b3cc24491bdce2b0db75cb853d909f
farrah-simpson <farrah_simpson@brown.edu>
2025-09-18 10:03:14 -0500

    trigger SFs

--- Trigger/Plots/plot_Trigger_nTrk.py
+++ Trigger/Plots/plot_Trigger_nTrk.py
@@ -13,7 +13,7 @@ import ctypes
 gStyle.SetOptStat(ROOT.kFALSE)
 parser=argparse.ArgumentParser()
 
-parser.add_argument("-d", "--data1", default="test_Run2023C_trigger.root", help="The rootfile for 2023 data") 
+parser.add_argument("-d", "--data1", default="test_Run2023D.root", help="The rootfile for 2023 data") 
 parser.add_argument("-d2", "--data2", default="DisplacedJets_Run2018All_Trigger.root", help="The rootfile for 2018 data")
 parser.add_argument("-m", "--mc", default="Trigger_OffSel_QCD_HT_Merged_New_92X.root", help="The rootfile for Run2 MC")
 
@@ -22,6 +22,8 @@ args = parser.parse_args()
 dataname = args.data1
 #data18name = args.data2
 mcname  = args.mc
+if "Run2023C" in dataname: run = "Run2023C"
+elif "Run2023D" in dataname: run = "Run2023D"
 
 can = TCanvas("can", "can", 800, 800)
 can.cd()
@@ -88,19 +90,19 @@ leg=ROOT.TLegend(0.3,0.65, 0.55,0.8)
 leg.SetLineColor(0)
 leg.SetFillColorAlpha(0,0)
 
-leg.AddEntry(gra_jetpt, "Run2023C", "pe")
+leg.AddEntry(gra_jetpt, "Run2023D", "pe")
 #leg.AddEntry(gra_jetpt_2018, "Run2018", "pe")
 leg.AddEntry(gra_jetpt_mc, "W+Jets selection MC", "pe")
 #leg.AddEntry(gra_PromptTrk_mc, "MC (QCD MultiJet)", "pe")
 leg.Draw("SAME")
 
-# --- Build ratio graph (data / MC) with errors ---
+# --- Build ratio_jetpt graph (data / MC) with errors ---
 n_data = gra_jetpt.GetN()
 n_mc   = gra_jetpt_mc.GetN()
 npoints = min(n_data, n_mc)
 
-ratio = ROOT.TGraphErrors(npoints)
-ratio.SetName("ratio")
+ratio_jetpt = ROOT.TGraphErrors(npoints)
+ratio_jetpt.SetName("ratio_jetpt")
 
 for i in range(npoints):
     # Prepare containers for GetPoint
@@ -122,40 +124,40 @@ for i in range(npoints):
     if ym_val > 0 and yd_val > 0:
         R = yd_val / ym_val
         err = R * ((ey_d / yd_val)**2 + (ey_m / ym_val)**2)**0.5
-        ratio.SetPoint(i, xd_val, R)
-        ratio.SetPointError(i, 0.0, err)
+        ratio_jetpt.SetPoint(i, xd_val, R)
+        ratio_jetpt.SetPointError(i, 0.0, err)
     else:
-        ratio.SetPoint(i, xd_val, 0.0)
-        ratio.SetPointError(i, 0.0, 0.0)
-# --- Style ratio plot ---
+        ratio_jetpt.SetPoint(i, xd_val, 0.0)
+        ratio_jetpt.SetPointError(i, 0.0, 0.0)
+# --- Style ratio_jetpt plot ---
 pad2_1.cd()
-ratio.SetTitle("")
-ratio.GetYaxis().SetTitle("Data/MC")
-ratio.GetYaxis().SetRangeUser(0.0, 2.0)
-ratio.GetYaxis().SetNdivisions(505)
-ratio.GetYaxis().SetTitleSize(0.12)
-ratio.GetYaxis().SetTitleOffset(0.5)
-ratio.GetYaxis().SetLabelSize(0.1)
+ratio_jetpt.SetTitle("")
+ratio_jetpt.GetYaxis().SetTitle("Data/MC")
+ratio_jetpt.GetYaxis().SetRangeUser(0.0, 3.0)
+ratio_jetpt.GetYaxis().SetNdivisions(505)
+ratio_jetpt.GetYaxis().SetTitleSize(0.12)
+ratio_jetpt.GetYaxis().SetTitleOffset(0.5)
+ratio_jetpt.GetYaxis().SetLabelSize(0.1)
 
-ratio.GetXaxis().SetTitle("Offline PF jet p_{T} [GeV]")
-ratio.GetXaxis().SetTitleSize(0.12)
-ratio.GetXaxis().SetLabelSize(0.1)
-ratio.GetXaxis().SetRangeUser(0.0,140.0)
+ratio_jetpt.GetXaxis().SetTitle("Offline PF jet p_{T} [GeV]")
+ratio_jetpt.GetXaxis().SetTitleSize(0.12)
+ratio_jetpt.GetXaxis().SetLabelSize(0.1)
+ratio_jetpt.GetXaxis().SetRangeUser(0.0,140.0)
 
-ratio.SetMarkerStyle(ROOT.kFullCircle)
-ratio.SetMarkerSize(0.75)
-ratio.Draw("APE")
+ratio_jetpt.SetMarkerStyle(ROOT.kFullCircle)
+ratio_jetpt.SetMarkerSize(0.75)
+ratio_jetpt.Draw("APE")
 
-# Draw reference line at ratio=1
-line = ROOT.TLine(0.0, 1.0, 140.0, 1.0)
+# Draw reference line at ratio_jetpt=1
+line = ROOT.TLine(0.0, 1.0, 130.0, 1.0)
 line.SetLineStyle(2)
 line.Draw("SAME")
 
 can.Update()
 
 
-can.SaveAs("Run2023C-trigger_CalojetPt40_Eff.png")
-can.SaveAs("Run2023C-trigger_CalojetPt40_Eff.pdf")
+can.SaveAs(run+"-trigger_CalojetPt40_Eff.png")
+can.SaveAs(run+"-trigger_CalojetPt40_Eff.pdf")
 #can.SaveAs("Run3_CalojetPt40_Eff.eps")
 #can.SaveAs("Run3_CalojetPt40_Eff.C")
 
@@ -165,7 +167,7 @@ can2.SetTickx()
 can2.SetTicky()
 can2.Divide(1,2)
 
-# Adjust pads (top for efficiency, bottom for ratio)
+# Adjust pads (top for efficiency, bottom for ratio_prompttrk)
 pad1_2 = can2.cd(1)
 pad1_2.SetPad(0.0, 0.3, 1.0, 1.0)   # xlow, ylow, xup, yup
 pad1_2.SetBottomMargin(0.02)        # reduce x-axis space
@@ -219,17 +221,17 @@ leg=ROOT.TLegend(0.5,0.70, 0.85,0.85)
 leg.SetLineColor(0)
 leg.SetFillColorAlpha(0,0)
 
-leg.AddEntry(gra_PromptTrk, "Run2023C", "pe")
+leg.AddEntry(gra_PromptTrk, run, "pe")
 #leg.AddEntry(gra_PromptTrk_2018, "Run2018", "pe")
 leg.AddEntry(gra_PromptTrk_mc, "W+Jets selection MC", "pe")
 leg.Draw("SAME")
-# --- Build ratio graph (data / MC) with errors ---
+# --- Build ratio_prompttrk graph (data / MC) with errors ---
 n_data = gra_PromptTrk.GetN()
 n_mc   = gra_PromptTrk_mc.GetN()
 npoints = min(n_data, n_mc)
 
-ratio = ROOT.TGraphErrors(npoints)
-ratio.SetName("ratio")
+ratio_prompttrk = ROOT.TGraphErrors(npoints)
+ratio_prompttrk.SetName("ratio_prompttrk")
 
 for i in range(npoints):
     # Prepare containers for GetPoint
@@ -251,40 +253,40 @@ for i in range(npoints):
     if ym_val > 0 and yd_val > 0:
         R = yd_val / ym_val
         err = R * ((ey_d / yd_val)**2 + (ey_m / ym_val)**2)**0.5
-        ratio.SetPoint(i, xd_val, R)
-        ratio.SetPointError(i, 0.0, err)
+        ratio_prompttrk.SetPoint(i, xd_val, R)
+        ratio_prompttrk.SetPointError(i, 0.0, err)
     else:
-        ratio.SetPoint(i, xd_val, 0.0)
-        ratio.SetPointError(i, 0.0, 0.0)
-# --- Style ratio plot ---
+        ratio_prompttrk.SetPoint(i, xd_val, 0.0)
+        ratio_prompttrk.SetPointError(i, 0.0, 0.0)
+# --- Style ratio_prompttrk plot ---
 pad2_2.cd()
-ratio.SetTitle("")
-ratio.GetYaxis().SetTitle("Data/MC")
-ratio.GetYaxis().SetRangeUser(0.0, 2.0)
-ratio.GetYaxis().SetNdivisions(505)
-ratio.GetYaxis().SetTitleSize(0.12)
-ratio.GetYaxis().SetTitleOffset(0.5)
-ratio.GetYaxis().SetLabelSize(0.1)
+ratio_prompttrk.SetTitle("")
+ratio_prompttrk.GetYaxis().SetTitle("Data/MC")
+ratio_prompttrk.GetYaxis().SetRangeUser(0.0, 3.0)
+ratio_prompttrk.GetYaxis().SetNdivisions(505)
+ratio_prompttrk.GetYaxis().SetTitleSize(0.12)
+ratio_prompttrk.GetYaxis().SetTitleOffset(0.5)
+ratio_prompttrk.GetYaxis().SetLabelSize(0.1)
 
-ratio.GetXaxis().SetTitle("Number of Offline Prompt Tracks")
-ratio.GetXaxis().SetTitleSize(0.12)
-ratio.GetXaxis().SetLabelSize(0.1)
-ratio.GetXaxis().SetRangeUser(-0.5,20.0)
+ratio_prompttrk.GetXaxis().SetTitle("Number of Offline Prompt Tracks")
+ratio_prompttrk.GetXaxis().SetTitleSize(0.12)
+ratio_prompttrk.GetXaxis().SetLabelSize(0.1)
+ratio_prompttrk.GetXaxis().SetRangeUser(-0.5,20.0)
 
-ratio.SetMarkerStyle(ROOT.kFullCircle)
-ratio.SetMarkerSize(0.75)
-ratio.Draw("APE")
+ratio_prompttrk.SetMarkerStyle(ROOT.kFullCircle)
+ratio_prompttrk.SetMarkerSize(0.75)
+ratio_prompttrk.Draw("APE")
 
-# Draw reference line at ratio=1
-line = ROOT.TLine(-0.5, 1.0, 20.0, 1.0)
+# Draw reference line at ratio_prompttrk=1
+line = ROOT.TLine(0., 1.0, 20.0, 1.0)
 line.SetLineStyle(2)
 line.Draw("SAME")
 
 can2.Update()
 
 
-can2.SaveAs("Run2023C-trigger_PromptTrack_Eff.png")
-can2.SaveAs("Run2023C-trigger_PromptTrack_Eff.pdf")
+can2.SaveAs(run+"-trigger_PromptTrack_Eff.png")
+can2.SaveAs(run+"-trigger_PromptTrack_Eff.pdf")
 #can.SaveAs("Run3_PromptTrack_Eff.eps")
 #can.SaveAs("Run3_PromptTrack_Eff.C")
 
@@ -293,7 +295,7 @@ can3.SetTickx()
 can3.SetTicky()
 can3.Divide(1,2)
 
-# Adjust pads (top for efficiency, bottom for ratio)
+# Adjust pads (top for efficiency, bottom for ratio_displacedtrk)
 pad1_3 = can3.cd(1)
 pad1_3.SetPad(0.0, 0.3, 1.0, 1.0)   # xlow, ylow, xup, yup
 pad1_3.SetBottomMargin(0.02)        # reduce x-axis space
@@ -347,18 +349,18 @@ leg=ROOT.TLegend(0.5,0.70, 0.85,0.85)
 leg.SetLineColor(0)
 leg.SetFillColorAlpha(0,0)
 
-leg.AddEntry(gra_DisplacedTrk, "Run2023C", "pe")
+leg.AddEntry(gra_DisplacedTrk, run, "pe")
 #leg.AddEntry(gra_DisplacedTrk_2018, "Run2018", "pe")
 leg.AddEntry(gra_DisplacedTrk_mc, "W+Jets selection MC", "pe")
 leg.Draw("SAME")
 
-# --- Build ratio graph (data / MC) with errors ---
+# --- Build ratio_displacedtrk graph (data / MC) with errors ---
 n_data = gra_DisplacedTrk.GetN()
 n_mc   = gra_DisplacedTrk_mc.GetN()
 npoints = min(n_data, n_mc)
 
-ratio = ROOT.TGraphErrors(npoints)
-ratio.SetName("ratio")
+ratio_displacedtrk = ROOT.TGraphErrors(npoints)
+ratio_displacedtrk.SetName("ratio_displacedtrk")
 
 for i in range(npoints):
     # Prepare containers for GetPoint
@@ -380,38 +382,66 @@ for i in range(npoints):
     if ym_val > 0 and yd_val > 0:
         R = yd_val / ym_val
         err = R * ((ey_d / yd_val)**2 + (ey_m / ym_val)**2)**0.5
-        ratio.SetPoint(i, xd_val, R)
-        ratio.SetPointError(i, 0.0, err)
+        ratio_displacedtrk.SetPoint(i, xd_val, R)
+        ratio_displacedtrk.SetPointError(i, 0.0, err)
     else:
-        ratio.SetPoint(i, xd_val, 0.0)
-        ratio.SetPointError(i, 0.0, 0.0)
-# --- Style ratio plot ---
+        ratio_displacedtrk.SetPoint(i, xd_val, 0.0)
+        ratio_displacedtrk.SetPointError(i, 0.0, 0.0)
+# --- Style ratio_displacedtrk plot ---
 pad2_3.cd()
-ratio.SetTitle("")
-ratio.GetYaxis().SetTitle("Data/MC")
-ratio.GetYaxis().SetRangeUser(0.0, 2.0)
-ratio.GetYaxis().SetNdivisions(505)
-ratio.GetYaxis().SetTitleSize(0.12)
-ratio.GetYaxis().SetTitleOffset(0.5)
-ratio.GetYaxis().SetLabelSize(0.1)
+ratio_displacedtrk.SetTitle("")
+ratio_displacedtrk.GetYaxis().SetTitle("Data/MC")
+ratio_displacedtrk.GetYaxis().SetRangeUser(0.0, 3.0)
+ratio_displacedtrk.GetYaxis().SetNdivisions(505)
+ratio_displacedtrk.GetYaxis().SetTitleSize(0.12)
+ratio_displacedtrk.GetYaxis().SetTitleOffset(0.5)
+ratio_displacedtrk.GetYaxis().SetLabelSize(0.1)
 
-ratio.GetXaxis().SetTitle("Number of Offline Displaced Tracks")
-ratio.GetXaxis().SetTitleSize(0.12)
-ratio.GetXaxis().SetLabelSize(0.1)
-ratio.GetXaxis().SetRangeUser(-0.5,12.5)
+ratio_displacedtrk.GetXaxis().SetTitle("Number of Offline Displaced Tracks")
+ratio_displacedtrk.GetXaxis().SetTitleSize(0.12)
+ratio_displacedtrk.GetXaxis().SetLabelSize(0.1)
+ratio_displacedtrk.GetXaxis().SetRangeUser(-0.5,12.5)
 
-ratio.SetMarkerStyle(ROOT.kFullCircle)
-ratio.SetMarkerSize(0.75)
-ratio.Draw("APE")
+ratio_displacedtrk.SetMarkerStyle(ROOT.kFullCircle)
+ratio_displacedtrk.SetMarkerSize(0.75)
+ratio_displacedtrk.Draw("APE")
 
-# Draw reference line at ratio=1
-line = ROOT.TLine(-0.5, 1.0, 12.5, 1.0)
+# Draw reference line at ratio_displacedtrk=1
+line = ROOT.TLine(0., 1.0, 12.5, 1.0)
 line.SetLineStyle(2)
 line.Draw("SAME")
 
 can3.Update()
 
-can3.SaveAs("Run2023C-trigger_DisplacedTrack_Eff.png")
-can3.SaveAs("Run2023C-trigger_DisplacedTrack_Eff.pdf")
+can3.SaveAs(run+"-trigger_DisplacedTrack_Eff.png")
+can3.SaveAs(run+"-trigger_DisplacedTrack_Eff.pdf")
 #can.SaveAs("Run3_DisplacedTrack_Eff.eps")
 #can.SaveAs("Run3_DisplacedTrack_Eff.C")
+
+threshold = 40.0
+
+# --- Open one file for all SFs ---
+with open(run+"scale_factors_PtrkShortSig5.txt", "w") as fout:
+    fout.write("# Scale factors (Data/MC)\n")
+    fout.write("# Each section corresponds to one observable\n\n")
+
+    # --- Jet pt scale factors ---
+    fout.write("[Offline PF jet pT (GeV)]\n")
+    fout.write("# pT\tRatio(Data/MC)\tStat. error\n")
+    for i in range(ratio_jetpt.GetN()):
+        x = ctypes.c_double()
+        y = ctypes.c_double()
+        ratio_jetpt.GetPoint(i, x, y)
+        if x.value < threshold: continue
+        fout.write(f"{x.value:.1f}\t{y.value:.3f}\t{ratio_jetpt.GetEY()[i]:.3f}\n")
+    fout.write("\n")
+
+    # --- Prompt track scale factors ---
+    fout.write("[Number of Offline Prompt Tracks]\n")
+    fout.write("# Ntrk\tRatio(Data/MC)\tStat. error\n")
+    for i in range(ratio_prompttrk.GetN()):
+        x = ctypes.c_double()
+        y = ctypes.c_double()
+        ratio_prompttrk.GetPoint(i, x, y)
+        fout.write(f"{x.value:.1f}\t{y.value:.3f}\t{ratio_prompttrk.GetEY()[i]:.3f}\n")
+    fout.write("\n")
